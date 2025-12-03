6d0b64a25c4bdbc7e83eacdc49b4e0866aa2aae9
AroneHaha <165986448+AroneHaha@users.noreply.github.com>
2025-12-03 22:31:35 +0800

    UPDATE: Working Gmail Message Fetch
    FIX: UI, Loading Time & Screen

--- BATODA/User Control Forms/CSUForm.Designer.cs
+++ BATODA/User Control Forms/CSUForm.Designer.cs
@@ -32,6 +32,15 @@
             this.label1 = new System.Windows.Forms.Label();
             this.InboxFlowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
             this.TESTPANEL = new System.Windows.Forms.Button();
+            this.MessagePanel = new System.Windows.Forms.Panel();
+            this.CloseMessage = new System.Windows.Forms.Button();
+            this.DateLbl = new System.Windows.Forms.Label();
+            this.FromLbl = new System.Windows.Forms.Label();
+            this.ContentTxt = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.MessagePanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // EditBodyNoPreviewLbl
@@ -72,17 +81,108 @@
             this.TESTPANEL.UseVisualStyleBackColor = true;
             this.TESTPANEL.Click += new System.EventHandler(this.TESTPANEL_Click);
             // 
+            // MessagePanel
+            // 
+            this.MessagePanel.Controls.Add(this.CloseMessage);
+            this.MessagePanel.Controls.Add(this.DateLbl);
+            this.MessagePanel.Controls.Add(this.FromLbl);
+            this.MessagePanel.Controls.Add(this.ContentTxt);
+            this.MessagePanel.Controls.Add(this.label4);
+            this.MessagePanel.Controls.Add(this.label3);
+            this.MessagePanel.Controls.Add(this.label2);
+            this.MessagePanel.Location = new System.Drawing.Point(504, 168);
+            this.MessagePanel.Name = "MessagePanel";
+            this.MessagePanel.Size = new System.Drawing.Size(694, 650);
+            this.MessagePanel.TabIndex = 1;
+            this.MessagePanel.Visible = false;
+            // 
+            // CloseMessage
+            // 
+            this.CloseMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CloseMessage.Location = new System.Drawing.Point(35, 17);
+            this.CloseMessage.Name = "CloseMessage";
+            this.CloseMessage.Size = new System.Drawing.Size(38, 34);
+            this.CloseMessage.TabIndex = 6;
+            this.CloseMessage.Text = "<";
+            this.CloseMessage.UseVisualStyleBackColor = true;
+            this.CloseMessage.Click += new System.EventHandler(this.CloseMessage_Click);
+            // 
+            // DateLbl
+            // 
+            this.DateLbl.AutoSize = true;
+            this.DateLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DateLbl.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.DateLbl.Location = new System.Drawing.Point(558, 65);
+            this.DateLbl.Name = "DateLbl";
+            this.DateLbl.Size = new System.Drawing.Size(100, 24);
+            this.DateLbl.TabIndex = 5;
+            this.DateLbl.Text = "12/25/2025";
+            // 
+            // FromLbl
+            // 
+            this.FromLbl.AutoSize = true;
+            this.FromLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FromLbl.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.FromLbl.Location = new System.Drawing.Point(86, 65);
+            this.FromLbl.Name = "FromLbl";
+            this.FromLbl.Size = new System.Drawing.Size(145, 24);
+            this.FromLbl.TabIndex = 4;
+            this.FromLbl.Text = "GmailUsername";
+            // 
+            // ContentTxt
+            // 
+            this.ContentTxt.Enabled = false;
+            this.ContentTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ContentTxt.Location = new System.Drawing.Point(35, 134);
+            this.ContentTxt.Name = "ContentTxt";
+            this.ContentTxt.Size = new System.Drawing.Size(623, 487);
+            this.ContentTxt.TabIndex = 3;
+            this.ContentTxt.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(31, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 24);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Content:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(513, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 24);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Date:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "From:";
+            // 
             // CSUForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.MessagePanel);
             this.Controls.Add(this.TESTPANEL);
             this.Controls.Add(this.InboxFlowLayoutPanel);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.EditBodyNoPreviewLbl);
             this.Name = "CSUForm";
             this.Size = new System.Drawing.Size(1768, 938);
+            this.MessagePanel.ResumeLayout(false);
+            this.MessagePanel.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -93,5 +193,13 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.FlowLayoutPanel InboxFlowLayoutPanel;
         private System.Windows.Forms.Button TESTPANEL;
+        private System.Windows.Forms.Panel MessagePanel;
+        private System.Windows.Forms.Button CloseMessage;
+        private System.Windows.Forms.Label DateLbl;
+        private System.Windows.Forms.Label FromLbl;
+        private System.Windows.Forms.RichTextBox ContentTxt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
     }
 }
